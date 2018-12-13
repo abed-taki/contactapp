3059fc589ea6074da409e1faf9baf1850a319a6d
abed-taki <pay2taki@gmail.com>
2018-12-13 17:46:52 +0100

    Updates

---
+++ precache-manifest.7d529d15450f6b960164afda0162a005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "adb34f852c58a9da01ea",
+    "url": "/contactapp/static/js/runtime~main.adb34f85.js"
+  },
+  {
+    "revision": "a7c559b3afb0d83d644a",
+    "url": "/contactapp/static/js/main.a7c559b3.chunk.js"
+  },
+  {
+    "revision": "edfb9a6b52cf1dc2b04d",
+    "url": "/contactapp/static/js/1.edfb9a6b.chunk.js"
+  },
+  {
+    "revision": "a7c559b3afb0d83d644a",
+    "url": "/contactapp/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "edfb9a6b52cf1dc2b04d",
+    "url": "/contactapp/static/css/1.5e33eff6.chunk.css"
+  },
+  {
+    "revision": "5264f7a7500e1d904ce48d45c9722400",
+    "url": "/contactapp/index.html"
+  }
+];
